27c8125baba8100b5fd1a9c757320a4ff84b991c
Diti2604 <ditiboyboy@gmail.com>
2024-06-17 17:42:12 +0200

    LoginView page done

---
+++ src/View/LoginView.java
@@ -0,0 +1,73 @@
+package View;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+
+public class LoginView extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Image image = new Image(getClass().getResource("/Assets/loginPicture.jpg").toExternalForm());
+        ImageView imageView = new ImageView(image);
+        imageView.setPreserveRatio(true);
+
+        TextField usernameTextField = new TextField();
+        usernameTextField.setPromptText("Enter username");
+        usernameTextField.setMaxWidth(200);
+
+        PasswordField passwordField = new PasswordField();
+        passwordField.setPromptText("Enter password");
+        passwordField.setMaxWidth(200);
+
+        Button loginButton = new Button("Log in");
+        loginButton.setStyle("-fx-border-radius: 8px;-fx-border-width:4px;  -fx-font-weight:bold; -fx-background-color:white;-fx-cursor:pointer;");
+        loginButton.setPrefHeight(20);
+
+        loginButton.setOnMouseEntered(e -> loginButton.setStyle("-fx-border-radius: 8px; -fx-border-width: 4px; -fx-font-weight: bold; -fx-background-color: white; -fx-cursor: hand;"));
+        loginButton.setOnMouseExited(e -> loginButton.setStyle("-fx-border-radius: 8px; -fx-border-width: 4px; -fx-font-weight: bold; -fx-background-color: white; -fx-cursor: pointer;"));
+
+
+        VBox loginPane = new VBox();
+        loginPane.setAlignment(Pos.CENTER);
+        loginPane.setPadding(new Insets(15));
+        loginPane.setSpacing(10);
+//        loginPane.setMaxWidth(300);
+        loginPane.getChildren().addAll(usernameTextField, passwordField, loginButton);
+
+        Rectangle rectangle = new Rectangle(300, 200);
+        rectangle.setFill(Color.rgb(0, 0, 0, 0.5));
+        rectangle.setArcHeight(20);
+        rectangle.setArcWidth(20);
+
+        StackPane rootPane = new StackPane();
+        rootPane.setStyle("-fx-background-color: #1e1e2e;");
+        rootPane.getChildren().addAll(imageView, rectangle, loginPane);
+        StackPane.setAlignment(rectangle, Pos.CENTER);
+        StackPane.setAlignment(loginPane, Pos.CENTER);
+
+        Scene scene = new Scene(rootPane, 800, 600);
+        stage.setScene(scene);
+        stage.setTitle("Login Page");
+        stage.setMaximized(true);
+
+        stage.show();
+    }
+}
